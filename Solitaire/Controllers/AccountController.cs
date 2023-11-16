e354604b3b9626d95e981be3e425e3dacbc6db4d
SimonX-bit <simon.groiss2002@gmail.com>
2023-11-16 21:33:20 +0100

    Add project files.

---
+++ Solitaire/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Solitaire.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Authenticate()
+        {
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register()
+        {
+            return NotFound();
+        }
+    }
+}
