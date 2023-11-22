3f2c8b3bf1f31a65a4f21074012a1e7b809eda59
SimonX-bit <simon.groiss2002@gmail.com>
2023-11-22 09:19:45 +0100

    add authorization in program.cs;

--- Solitaire/Program.cs
+++ Solitaire/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
 {
@@ -57,7 +58,6 @@ builder.Services
             )
         };
     });
-
 
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
