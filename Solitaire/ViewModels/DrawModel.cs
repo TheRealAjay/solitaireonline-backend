6b8e85d14ab8af8dec5d53603abb0caa7aefec99
SimonX-bit <simon.groiss2002@gmail.com>
2023-11-22 14:47:35 +0100

    Added DrawModel;

---
+++ Solitaire/ViewModels/DrawModel.cs
@@ -0,0 +1,15 @@
+﻿using Solitaire.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Solitaire.ViewModels
+{
+    public class DrawModel
+    {
+        public int Id { get; set; }
+        public int Sort { get; set; }
+        public string FromPosition { get; set; } = null!;
+        public string ToPosition { get; set; } = null!;
+
+        public virtual SolitaireSession SolitaireSession { get; set; }
+    }
+}
