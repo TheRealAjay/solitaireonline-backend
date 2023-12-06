4cff3513cf918e966d0056159db38c991c061849
SimonX-bit <simon.groiss2002@gmail.com>
2023-12-06 10:06:27 +0100

    Added migration for wasFlipped in drawModel;

---
+++ Solitaire.DataAccess/Migrations/20231206090543_AddedWasFlippedToDraw.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Solitaire.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedWasFlippedToDraw : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "WasFlipped",
+                table: "Draws",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WasFlipped",
+                table: "Draws");
+        }
+    }
+}
